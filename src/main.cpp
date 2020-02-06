589e0ab533fc0c3eb637efdeec1b0fb607c003e8
Oskar <57748177+JoeMamasXD@users.noreply.github.com>
2020-02-06 16:13:07 +0100

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+
+const char*  ssid ="Shreks andra Nätverk";
+const char* password = "Pepparkakan";
 
 void setup() {
-  // put your setup code here, to run once:
+
+Serial.begin(9600);
+
+WiFi.begin(ssid,password);
+Serial.println("Trying to connect to WiFi");
+while(WiFi.status() !=WL_CONNECTED){
+delay(500);
+Serial.println(".");
+}
+Serial.println("Succesfully connected to WiFi");
+
 }
 
 void loop() {
